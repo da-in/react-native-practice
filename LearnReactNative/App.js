7e67e74188bd0ae38f6ebfc2f894b0edda8c852e
da-in <talown@naver.com>
2022-06-26 14:14:55 +0900

    1.5.0 Setting ESLint & Prettier

--- LearnReactNative/App.js
+++ LearnReactNative/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  Text,
-  View,
-} from 'react-native';
+import {SafeAreaView, Text, View} from 'react-native';
 
 const App = () => {
   return (
     <SafeAreaView>
-        <View>
-          <Text>Hello React!</Text>
-        </View>
+      <View>
+        <Text>Hello React!</Text>
+      </View>
     </SafeAreaView>
   );
 };
