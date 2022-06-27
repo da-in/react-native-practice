afb60f54db9156398d9f0ca2d6e4498b3e11385e
da-in <talown@naver.com>
2022-06-27 19:04:48 +0900

    3.2.0 Build ToDoApp UI

--- TodoApp/components/AddTodo.js
+++ TodoApp/components/AddTodo.js
@@ -1,14 +1,71 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Image,
+  TouchableNativeFeedback,
+  Keyboard,
+} from 'react-native';
 
 function AddTodo() {
-  return <View style={styles.block} />;
+  const [text, setText] = useState('');
+
+  const onPress = () => {
+    setText('');
+    Keyboard.dismiss();
+  };
+
+  return (
+    <View style={styles.block}>
+      <TextInput
+        placeholder="할일을 입력하세요."
+        style={styles.input}
+        value={text}
+        onChangeText={setText}
+        onSubmitEditing={onPress}
+        returnKeyType="done"
+      />
+      <View style={styles.circleWrapper}>
+        <TouchableNativeFeedback activeOpacity={0.5} onPress={onPress}>
+          <View style={styles.buttonStyle}>
+            <Image
+              source={require('../assets/icons/add_white/add_white.png')}
+            />
+          </View>
+        </TouchableNativeFeedback>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   block: {
+    backgroundColor: 'white',
     height: 64,
-    backgroundColor: 'red',
+    paddingHorizontal: 16,
+    borderColor: '#bdbdbd',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    paddingVertical: 8,
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 48,
+    height: 48,
+    backgroundColor: '#26a69a',
+    borderRadius: 24,
+  },
+  circleWrapper: {
+    overflow: 'hidden',
+    borderRadius: 24,
   },
 });
 
