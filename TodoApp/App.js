1884de3b97da6dcb571b1a43015a69e5aef062ac
da-in <talown@naver.com>
2022-06-27 20:42:10 +0900

    4.3.0 Add Todo Item

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -7,18 +7,28 @@ import TodoList from './components/TodoList';
 
 function App() {
   const today = new Date();
-
   const [todos, setTodos] = useState([
     {id: 1, text: '작업환경 설정', done: true},
     {id: 2, text: '리액트 네이티브 기초 공부', done: false},
     {id: 3, text: '투두리스트 만들어보기', done: false},
   ]);
 
+  const onInsert = text => {
+    const nextId =
+      todos.length > 0 ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+    const todo = {
+      id: nextId,
+      text,
+      done: false,
+    };
+    setTodos(todos.concat(todo));
+  };
+
   return (
     <SafeAreaView style={styles.block}>
       <DateHead date={today} />
       {todos.length === 0 ? <Empty /> : <TodoList todos={todos} />}
-      <AddTodo />
+      <AddTodo onInsert={onInsert} />
     </SafeAreaView>
   );
 }
