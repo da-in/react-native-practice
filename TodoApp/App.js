1804b98dff5a5eb3464f3591f16e651d06ed3d23
da-in <talown@naver.com>
2022-06-28 16:28:12 +0900

    4.6.0 AsyncStorage

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -1,9 +1,10 @@
-import React, {useState} from 'react';
-import {SafeAreaView, Text, View, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
 import AddTodo from './components/AddTodo';
 import DateHead from './components/DateHead';
 import Empty from './components/Empty';
 import TodoList from './components/TodoList';
+import todosStorage from './storages/todosStorages';
 
 function App() {
   const today = new Date();
@@ -12,6 +13,14 @@ function App() {
     {id: 2, text: '리액트 네이티브 기초 공부', done: false},
     {id: 3, text: '투두리스트 만들어보기', done: false},
   ]);
+
+  useEffect(() => {
+    todosStorage.get().then(setTodos).catch(console.error);
+  }, []);
+
+  useEffect(() => {
+    todosStorage.set(todos).catch(console.error);
+  }, [todos]);
 
   const onInsert = text => {
     const nextId =
