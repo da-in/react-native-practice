d2fd0e39556050629b6542f49351f541f0440936
da-in <talown@naver.com>
2022-07-05 15:49:52 +0900

    Open WriteScreen When FeedListItem Pressed

--- DayLog/components/FeedListItem.js
+++ DayLog/components/FeedListItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Platform, Pressable, StyleSheet, Text} from 'react-native';
 import {format, formatDistanceToNow} from 'date-fns';
 import {ko} from 'date-fns/locale';
+import {useNavigation} from '@react-navigation/native';
 
 function truncate(text) {
   const replaced = text.replace(/\n/g, ' ');
@@ -26,13 +27,18 @@ function formatDate(date) {
 
 function FeedListItem({log}) {
   const {title, body, date} = log;
+  const navigation = useNavigation();
+  const onPress = () => {
+    navigation.navigate('Write', {log});
+  };
   return (
     <Pressable
       style={({pressed}) => [
         styles.block,
         Platform.OS === 'ios' && pressed && {backgroundColor: '#efefef'},
       ]}
-      android_ripple={{color: '#ededed'}}>
+      android_ripple={{color: '#ededed'}}
+      onPress={onPress}>
       <Text style={styles.date}>{formatDate(date)}</Text>
       <Text style={styles.title}>{title}</Text>
       <Text style={styles.body}>{truncate(body)}</Text>
