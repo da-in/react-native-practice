97300c0f3a5e0356e3807f0c352749d8126884e7
da-in <talown@naver.com>
2022-07-05 16:31:39 +0900

    7.3 Delete

--- DayLog/components/WriteHeader.js
+++ DayLog/components/WriteHeader.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import TransparentCircleButton from './TransparentCircleButton';
 
-function WriteHeader({onSave}) {
+function WriteHeader({onSave, onAskRemove, isEditing}) {
   const navigation = useNavigation();
   const onGoBack = () => {
     navigation.pop();
@@ -19,11 +19,14 @@ function WriteHeader({onSave}) {
       </View>
       <View style={styles.buttons}>
         <View style={[styles.iconButtonWrapper, styles.marginRight]}>
-          <TransparentCircleButton
-            name="delete-forever"
-            color="#ef5350"
-            hasMarginRight
-          />
+          {isEditing && (
+            <TransparentCircleButton
+              name="delete-forever"
+              color="#ef5350"
+              hasMarginRight
+              onPress={onAskRemove}
+            />
+          )}
         </View>
         <View style={styles.iconButtonWrapper}>
           <TransparentCircleButton
