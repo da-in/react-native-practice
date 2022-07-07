a46af9c49795e3b9b4c0dcdbb992bddd9226aff0
da-in <talown@naver.com>
2022-07-07 14:28:26 +0900

    8.2.4 SignInScreen Divide Component

--- PublicGalleryDain/screens/SignInScreen.js
+++ PublicGalleryDain/screens/SignInScreen.js
@@ -1,43 +1,35 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Keyboard, StyleSheet, Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import BorderedInput from '../components/BorderedInput';
-import CustomButton from '../components/CustomButton';
+import SignButton from '../components/SignButton';
+import SignForm from '../components/SignForm';
 
 function SingInScreen({navigation, route}) {
   const {isSignUp} = route.params ?? {};
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const createChangeTextHandler = name => value => {
+    setForm({...form, [name]: value});
+  };
+  const onSubmit = () => {
+    Keyboard.dismiss();
+    console.log(form);
+  };
+
   return (
     <SafeAreaView style={styles.fullscreen}>
       <Text style={styles.text}>PublicGallery</Text>
       <View style={styles.form}>
-        <BorderedInput hasMarginBottom placeholder="이메일" />
-        <BorderedInput placeholder="비밀번호" hasMarginBottom={isSignUp} />
-        {isSignUp && <BorderedInput placeholder="비밀번호 확인" />}
-        <View style={styles.buttons}>
-          {isSignUp ? (
-            <>
-              <CustomButton title="회원가입" hasMarginBottom />
-              <CustomButton
-                title="로그인"
-                theme="secondary"
-                onPress={() => {
-                  navigation.goBack();
-                }}
-              />
-            </>
-          ) : (
-            <>
-              <CustomButton title="로그인" hasMarginBottom />
-              <CustomButton
-                title="회원가입"
-                theme="secondary"
-                onPress={() => {
-                  navigation.push('SignIn', {isSignUp: true});
-                }}
-              />
-            </>
-          )}
-        </View>
+        <SignForm
+          form={form}
+          createChangeTextHandler={createChangeTextHandler}
+          isSignUp={isSignUp}
+          onSubmit={onSubmit}
+        />
+        <SignButton isSignUp={isSignUp} onSubmit={onSubmit} />
       </View>
     </SafeAreaView>
   );
@@ -57,9 +49,6 @@ const styles = StyleSheet.create({
     marginTop: 64,
     width: '100%',
     paddingHorizontal: 16,
-  },
-  buttons: {
-    marginTop: 64,
   },
 });
 
