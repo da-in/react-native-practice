1c25754042d00200be52b959b7151a60efa8b64b
da-in <talown@naver.com>
2022-07-07 17:06:18 +0900

    8.3 Add Firestore User Profile

--- PublicGalleryDain/screens/RootStack.js
+++ PublicGalleryDain/screens/RootStack.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SingInScreen from './SignInScreen';
+import WelcomeScreen from './WelcomeScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,11 @@ function RootStack() {
       <Stack.Screen
         name="SignIn"
         component={SingInScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Welcome"
+        component={WelcomeScreen}
         options={{headerShown: false}}
       />
     </Stack.Navigator>
--- PublicGalleryDain/screens/SignInScreen.js
+++ PublicGalleryDain/screens/SignInScreen.js
@@ -4,6 +4,7 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import SignButton from '../components/SignButton';
 import SignForm from '../components/SignForm';
 import {signIn, signUp} from '../lib/auth';
+import {getUser} from '../lib/users';
 
 function SingInScreen({navigation, route}) {
   const {isSignUp} = route.params ?? {};
@@ -30,7 +31,12 @@ function SingInScreen({navigation, route}) {
     setLoading(true);
     try {
       const {user} = isSignUp ? await signUp(info) : await signIn(info);
-      console.log(user);
+      const profile = await getUser(user.uid);
+      if (!profile) {
+        navigation.navigate('Welcome', {uid: user.uid});
+      } else {
+        // 구현 예정
+      }
     } catch (e) {
       const messages = {
         'auth/email-already-in-use': '이미 가입된 이메일입니다.',
