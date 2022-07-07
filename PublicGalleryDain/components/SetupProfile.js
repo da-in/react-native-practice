ebdebdd654a326647c1a5938370add62dc9569c0
da-in <talown@naver.com>
2022-07-07 19:08:02 +0900

    8.5.2 Default Profile Image

--- PublicGalleryDain/components/SetupProfile.js
+++ PublicGalleryDain/components/SetupProfile.js
@@ -55,12 +55,11 @@ function SetupProfile() {
       <Pressable onPress={onSelectImage}>
         <Image
           style={styles.circle}
-          source={{uri: response?.assets[0]?.uri}}
-          // source={
-          //   response
-          //     ? {uri: response?.assets[0]?.uri}
-          //     : require('../assets/user.png')
-          // }
+          source={
+            response
+              ? {uri: response?.assets[0]?.uri}
+              : require('../assets/user.png')
+          }
         />
       </Pressable>
       <View style={styles.form}>
