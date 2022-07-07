e7b77038d0b794fda03783debab42435d5cf8168
da-in <talown@naver.com>
2022-07-07 15:19:09 +0900

    8.2.6 Auth Exeption Handling

--- PublicGalleryDain/components/SignButton.js
+++ PublicGalleryDain/components/SignButton.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {ActivityIndicator, StyleSheet, View} from 'react-native';
 import CustomButton from './CustomButton';
 import {useNavigation} from '@react-navigation/native';
 
-function SignButton({isSignUp, onSubmit}) {
+function SignButton({isSignUp, onSubmit, loading}) {
   const navigation = useNavigation();
   const primaryTitle = isSignUp ? '회원가입' : '로그인';
   const secondaryTitle = isSignUp ? '로그인' : '회원가입';
@@ -16,6 +16,13 @@ function SignButton({isSignUp, onSubmit}) {
     }
   };
 
+  if (loading) {
+    return (
+      <View style={styles.spinnerWrapper}>
+        <ActivityIndicator size={32} color="#6200ee" />
+      </View>
+    );
+  }
   return (
     <>
       <View style={styles.buttons}>
@@ -33,6 +40,12 @@ function SignButton({isSignUp, onSubmit}) {
 const styles = StyleSheet.create({
   buttons: {
     marginTop: 64,
+  },
+  spinnerWrapper: {
+    marginTop: 64,
+    height: 104,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
