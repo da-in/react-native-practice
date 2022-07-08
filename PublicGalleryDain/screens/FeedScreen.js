9207944835b0c1c8803109be473eec369def5faf
da-in <talown@naver.com>
2022-07-08 20:25:03 +0900

    9.4.4.1 Sorting & Loading Limit

--- PublicGalleryDain/screens/FeedScreen.js
+++ PublicGalleryDain/screens/FeedScreen.js
@@ -1,8 +1,62 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, FlatList, StyleSheet, View} from 'react-native';
+import PostCard from '../components/PostCard';
+import {getOlderPosts, getPosts, PAGE_SIZE} from '../lib/post';
 
 function FeedScreen() {
-  return <View />;
+  const [posts, setPosts] = useState(null);
+  const [noMorePost, setNoMorePost] = useState(false);
+
+  useEffect(() => {
+    getPosts().then(setPosts);
+  }, []);
+
+  const onLoadMore = async () => {
+    if (noMorePost || !posts || posts.length < PAGE_SIZE) {
+      return;
+    }
+    const lastPost = posts[posts.length - 1];
+    const olderPosts = await getOlderPosts(lastPost.id);
+    if (olderPosts.length < PAGE_SIZE) {
+      setNoMorePost(true);
+    }
+    setPosts(posts.concat(olderPosts));
+  };
+
+  return (
+    <FlatList
+      data={posts}
+      renderItem={renderItem}
+      keyExtractor={item => item.id}
+      contentContainerStyle={styles.container}
+      onEndReached={onLoadMore}
+      onEndReachedThreshold={0.75}
+      ListFooterComponent={
+        !noMorePost && (
+          <ActivityIndicator style={styles.spinner} size={32} color="#6200ee" />
+        )
+      }
+    />
+  );
 }
+
+const renderItem = ({item}) => (
+  <PostCard
+    createdAt={item.createdAt}
+    description={item.description}
+    id={item.id}
+    user={item.user}
+    photoURL={item.photoURL}
+  />
+);
+
+const styles = StyleSheet.create({
+  container: {
+    paddingBottom: 48,
+  },
+  spinner: {
+    height: 64,
+  },
+});
 
 export default FeedScreen;
