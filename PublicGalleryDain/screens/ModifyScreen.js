90d92d7972326ebb692e6d74f9087a92ba224080
da-in <talown@naver.com>
2022-07-09 22:28:15 +0900

    9.6.4 Modify(Update) Post

---
+++ PublicGalleryDain/screens/ModifyScreen.js
@@ -0,0 +1,61 @@
+import {useNavigation, useRoute} from '@react-navigation/native';
+import React, {useCallback, useEffect, useState} from 'react';
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  TextInput,
+  Platform,
+} from 'react-native';
+import IconRightButton from '../components/IconRightButton';
+import {updatePost} from '../lib/post';
+
+function ModifyScreen() {
+  const navigation = useNavigation();
+  const {params} = useRoute();
+  const [description, setDescription] = useState(params.description);
+
+  const onSubmit = useCallback(async () => {
+    await updatePost({
+      id: params.id,
+      description,
+    });
+    navigation.pop();
+  }, [navigation, params.id, description]);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <IconRightButton onPress={onSubmit} name="check" />,
+    });
+  });
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.select({ios: 'height'})}
+      style={styles.block}
+      keyboardVerticalOffset={Platform.select({ios: 88})}>
+      <TextInput
+        style={styles.input}
+        multiline={true}
+        placeholder="이 사진에 대한 설명을 입력하세요..."
+        textAlignVertical="top"
+        value={description}
+        onChangeText={setDescription}
+      />
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+  },
+  input: {
+    paddingHorizontal: 16,
+    paddingTop: 16,
+    paddingBottom: 16,
+    flex: 1,
+    fontSize: 16,
+  },
+});
+
+export default ModifyScreen;
