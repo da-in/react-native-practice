f94978576b6b039fb0e1b3b6f2d888af6bbec14b
da-in <talown@naver.com>
2022-07-10 17:04:02 +0900

    9.8 SettingScreen

--- PublicGalleryDain/screens/MyProfileScreen.js
+++ PublicGalleryDain/screens/MyProfileScreen.js
@@ -1,6 +1,7 @@
 import {useNavigation} from '@react-navigation/native';
 import React, {useEffect} from 'react';
 import {View} from 'react-native';
+import IconRightButton from '../components/IconRightButton';
 import Profile from '../components/Profile';
 import {useUserContext} from '../contexts/UserContext';
 
@@ -10,6 +11,12 @@ function MyProfileScreen() {
   useEffect(() => {
     navigation.setOptions({
       title: user.displayName,
+      headerRight: () => (
+        <IconRightButton
+          name="settings"
+          onPress={() => navigation.push('Setting')}
+        />
+      ),
     });
   }, [navigation, user]);
   return <Profile userId={user.id} />;
